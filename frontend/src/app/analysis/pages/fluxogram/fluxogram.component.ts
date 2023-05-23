ac95f6a30bfb4ac6643b1c503cc3157b09d896cf
Júlia Dias <jedl@cin.ufpe.br>
2023-05-23 17:33:13 -0300

    feat: basic fluxogram data presentation

--- frontend/src/app/analysis/pages/fluxogram/fluxogram.component.ts
+++ frontend/src/app/analysis/pages/fluxogram/fluxogram.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import * as d3 from 'd3';
 
 @Component({
   selector: 'app-fluxogram',
   templateUrl: './fluxogram.component.html',
-  styleUrls: ['./fluxogram.component.scss']
+  styleUrls: ['./fluxogram.component.scss'],
 })
 export class FluxogramComponent {
+  svg: any;
 
+  movements_count = 0;
+  process_count = 0;
+  group_process_count = 0;
+  avg_process_duration = 0;
+
+  ngOnInit(): void {
+    this.createSvg();
+  }
+
+  private createSvg(): void {
+    d3.select('figure#fluxogram > *').remove();
+    this.svg = d3.select('figure#fluxogram').append('svg').append('g');
+  }
 }
