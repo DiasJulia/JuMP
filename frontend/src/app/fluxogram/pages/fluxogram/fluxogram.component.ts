dcd2066f76f9b23b45acfc2ec23c05460a0afa14
Júlia Dias <jedl@cin.ufpe.br>
2023-05-25 16:23:39 -0300

    fix: read proper time from average case duration

--- frontend/src/app/fluxogram/pages/fluxogram/fluxogram.component.ts
+++ frontend/src/app/fluxogram/pages/fluxogram/fluxogram.component.ts
@@ -11,14 +11,44 @@ import { FluxogramFacade } from '../../fluxogram.facade';
 export class FluxogramComponent {
   svg: any;
 
-  avgCaseDuration: Date = new Date();
+  avgCaseDuration = { anos: 0, meses: 0, dias: 0, horas: 0, minutos: 0 };
 
   processoStats: ProcessoStats = <ProcessoStats>{};
 
   constructor(facade: FluxogramFacade) {
     facade.getProcessoStats().subscribe((processoStats) => {
       this.processoStats = processoStats[0];
-      this.avgCaseDuration = new Date(this.processoStats.avgCaseDuration);
+      this.avgCaseDuration.anos = Math.floor(
+        this.processoStats.avgCaseDuration / 31104000
+      );
+
+      this.processoStats.avgCaseDuration -=
+        this.avgCaseDuration.anos * 31104000;
+
+      this.avgCaseDuration.meses = Math.floor(
+        this.processoStats.avgCaseDuration / 2592000
+      );
+
+      this.processoStats.avgCaseDuration -=
+        this.avgCaseDuration.meses * 2592000;
+
+      this.avgCaseDuration.dias = Math.floor(
+        this.processoStats.avgCaseDuration / 86400
+      );
+
+      this.processoStats.avgCaseDuration -= this.avgCaseDuration.dias * 86400;
+
+      this.avgCaseDuration.horas =
+        Math.floor(this.processoStats.avgCaseDuration / 3600) % 24;
+
+      this.processoStats.avgCaseDuration -=
+        Math.floor(this.processoStats.avgCaseDuration / 3600) * 3600;
+
+      this.avgCaseDuration.minutos =
+        Math.floor(this.processoStats.avgCaseDuration / 60) % 60;
+
+      this.processoStats.avgCaseDuration -=
+        Math.floor(this.processoStats.avgCaseDuration / 60) * 60;
     });
   }
 
