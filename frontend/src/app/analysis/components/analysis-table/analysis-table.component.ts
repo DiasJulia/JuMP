1fcc0d2e601084dd05b70d32924d56e49a7594df
Júlia Dias <jedl@cin.ufpe.br>
2023-05-25 19:46:04 -0300

    feat: improve getTimeFromSeconds function

--- frontend/src/app/analysis/components/analysis-table/analysis-table.component.ts
+++ frontend/src/app/analysis/components/analysis-table/analysis-table.component.ts
@@ -52,27 +52,38 @@ export class AnalysisTableComponent
   }
 
   public getTimeFromSeconds(seconds: number) {
+    let response = '';
     let caseDuration = { anos: 0, meses: 0, dias: 0, horas: 0, minutos: 0 };
     caseDuration.anos = Math.floor(seconds / 31104000);
-
-    seconds -= caseDuration.anos * 31104000;
+    if (caseDuration.anos > 0) {
+      seconds -= caseDuration.anos * 31104000;
+      response += `${caseDuration.anos} anos, `;
+    }
 
     caseDuration.meses = Math.floor(seconds / 2592000);
-
-    seconds -= caseDuration.meses * 2592000;
+    if (caseDuration.meses > 0) {
+      seconds -= caseDuration.meses * 2592000;
+      response += `${caseDuration.meses} meses, `;
+    }
 
     caseDuration.dias = Math.floor(seconds / 86400);
-
-    seconds -= caseDuration.dias * 86400;
+    if (caseDuration.dias > 0) {
+      seconds -= caseDuration.dias * 86400;
+      response += `${caseDuration.dias} dias, `;
+    }
 
     caseDuration.horas = Math.floor(seconds / 3600) % 24;
-
-    seconds -= Math.floor(seconds / 3600) * 3600;
+    if (caseDuration.horas > 0) {
+      seconds -= Math.floor(seconds / 3600) * 3600;
+      response += `${caseDuration.horas} horas, `;
+    }
 
     caseDuration.minutos = Math.floor(seconds / 60) % 60;
+    if (caseDuration.minutos > 0) {
+      seconds -= Math.floor(seconds / 60) * 60;
+      response += `${caseDuration.minutos} minutos`;
+    }
 
-    seconds -= Math.floor(seconds / 60) * 60;
-
-    return `${caseDuration.anos} anos, ${caseDuration.meses} meses, ${caseDuration.dias} dias, ${caseDuration.horas} horas, ${caseDuration.minutos} minutos`;
+    return response;
   }
 }
