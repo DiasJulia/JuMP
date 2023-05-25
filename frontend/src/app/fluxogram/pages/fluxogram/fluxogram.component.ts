f7dd6e9834e7a82039054b6c499505f61436e54f
Júlia Dias <jedl@cin.ufpe.br>
2023-05-25 01:30:32 -0300

    feat: show average case duration as monhs and days

--- frontend/src/app/fluxogram/pages/fluxogram/fluxogram.component.ts
+++ frontend/src/app/fluxogram/pages/fluxogram/fluxogram.component.ts
@@ -14,7 +14,7 @@ export class FluxogramComponent {
   movements_count = 0;
   process_count = 0;
   group_process_count = 0;
-  avg_process_duration = 0;
+  avgCaseDuration: Date = new Date();
 
   processoStats: ProcessoStats = {
     casesCount: 0,
@@ -27,6 +27,21 @@ export class FluxogramComponent {
   constructor(facade: FluxogramFacade) {
     facade.getProcessoStats().subscribe((processoStats) => {
       this.processoStats = processoStats[0];
+      const caseDuration = new Date(this.processoStats.avgCaseDuration);
+      const baseDate = new Date(0);
+      this.avgCaseDuration.setFullYear(
+        caseDuration.getFullYear() - baseDate.getFullYear()
+      );
+      this.avgCaseDuration.setMonth(
+        caseDuration.getMonth() - baseDate.getMonth()
+      );
+      this.avgCaseDuration.setDate(caseDuration.getDate() - baseDate.getDate());
+      this.avgCaseDuration.setHours(
+        caseDuration.getHours() - baseDate.getHours()
+      );
+      this.avgCaseDuration.setMinutes(
+        caseDuration.getMinutes() - baseDate.getMinutes()
+      );
     });
   }
 
