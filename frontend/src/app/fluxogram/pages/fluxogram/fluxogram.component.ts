7aa7857ec499c601c98e20c8c9f31f767f4a7613
Júlia Dias <jedl@cin.ufpe.br>
2023-05-25 16:07:05 -0300

    fix: remove previous date function

--- frontend/src/app/fluxogram/pages/fluxogram/fluxogram.component.ts
+++ frontend/src/app/fluxogram/pages/fluxogram/fluxogram.component.ts
@@ -11,37 +11,14 @@ import { FluxogramFacade } from '../../fluxogram.facade';
 export class FluxogramComponent {
   svg: any;
 
-  movements_count = 0;
-  process_count = 0;
-  group_process_count = 0;
   avgCaseDuration: Date = new Date();
 
-  processoStats: ProcessoStats = {
-    casesCount: 0,
-    movimentosCount: 0,
-    avgCaseDuration: 0,
-    avgMovimentoDuration: 0,
-    avgMovimentosPerCase: 0,
-  };
+  processoStats: ProcessoStats = <ProcessoStats>{};
 
   constructor(facade: FluxogramFacade) {
     facade.getProcessoStats().subscribe((processoStats) => {
       this.processoStats = processoStats[0];
-      const caseDuration = new Date(this.processoStats.avgCaseDuration);
-      const baseDate = new Date(0);
-      this.avgCaseDuration.setFullYear(
-        caseDuration.getFullYear() - baseDate.getFullYear()
-      );
-      this.avgCaseDuration.setMonth(
-        caseDuration.getMonth() - baseDate.getMonth()
-      );
-      this.avgCaseDuration.setDate(caseDuration.getDate() - baseDate.getDate());
-      this.avgCaseDuration.setHours(
-        caseDuration.getHours() - baseDate.getHours()
-      );
-      this.avgCaseDuration.setMinutes(
-        caseDuration.getMinutes() - baseDate.getMinutes()
-      );
+      this.avgCaseDuration = new Date(this.processoStats.avgCaseDuration);
     });
   }
 
