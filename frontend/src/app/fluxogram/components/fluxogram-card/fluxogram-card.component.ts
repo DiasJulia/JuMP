5cec150d70239ab90eb9066bb710189753d1dc62
Júlia Dias <jedl@cin.ufpe.br>
2023-05-26 12:55:44 -0300

    feat: implemented fluxogram as draggable inside card

--- frontend/src/app/fluxogram/components/fluxogram-card/fluxogram-card.component.ts
+++ frontend/src/app/fluxogram/components/fluxogram-card/fluxogram-card.component.ts
@@ -10,13 +10,50 @@ import * as d3 from 'd3';
 export class FluxogramCardComponent {
   svg: any;
 
-  constructor(private elementRef: ElementRef, imageApi: ImageApiService) {
+  xStart: number = 0;
+  yStart: number = 0;
+
+  xPosition: number = 0;
+  yPosition: number = 0;
+
+  constructor(
+    private elementRef: ElementRef,
+    private imageApi: ImageApiService
+  ) {
     imageApi.getFlowGraph().subscribe((data) => {
       this.svg = data;
       const container = d3
         .select(this.elementRef.nativeElement)
         .select('#fluxogram');
-      container.append('section').classed('svg-container', true).html(this.svg);
+      container
+        .append('section')
+        .classed('svg-container', true)
+        .attr('width', '80%')
+        .attr('height', '80%')
+        .html(this.svg);
+
+      this.svg = container.select('svg');
+      this.svg.call(
+        d3
+          .drag<any, any>()
+          .on('start', (event, d) => this.dragStart(event, d))
+          .on('drag', (event, d) => this.dragHandler(event, d))
+      );
     });
   }
+
+  dragStart = (event: any, d: any) => {
+    this.xStart = event.x;
+    this.yStart = event.y;
+  };
+
+  dragHandler = (event: any, d: any) => {
+    this.xPosition = event.x - this.xStart;
+    this.yPosition = event.y - this.yStart;
+
+    d3.select(this.svg.node()).attr(
+      'transform',
+      `translate(${this.xPosition}, ${this.yPosition})`
+    );
+  };
 }
