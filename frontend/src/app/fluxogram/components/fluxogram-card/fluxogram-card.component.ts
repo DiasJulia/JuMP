7a1a15fe5ef7b5af841bc902d6ae083190e9d684
Júlia Dias <jedl@cin.ufpe.br>
2023-05-26 13:50:11 -0300

    feat: draw information icon in nodes

--- frontend/src/app/fluxogram/components/fluxogram-card/fluxogram-card.component.ts
+++ frontend/src/app/fluxogram/components/fluxogram-card/fluxogram-card.component.ts
@@ -20,7 +20,7 @@ export class FluxogramCardComponent {
     private elementRef: ElementRef,
     private imageApi: ImageApiService
   ) {
-    imageApi.getFlowGraph().subscribe((data) => {
+    this.imageApi.getFlowGraph().subscribe((data) => {
       this.svg = data;
       const container = d3
         .select(this.elementRef.nativeElement)
@@ -39,6 +39,8 @@ export class FluxogramCardComponent {
           .on('start', (event, d) => this.dragStart(event, d))
           .on('drag', (event, d) => this.dragHandler(event, d))
       );
+
+      this.drawIconInNodes();
     });
   }
 
@@ -55,5 +57,29 @@ export class FluxogramCardComponent {
       'transform',
       `translate(${this.xPosition}, ${this.yPosition})`
     );
+  };
+
+  drawIconInNodes = () => {
+    let nodes = d3.selectAll('.node');
+    nodes.each((d: any, i: any, n: any) => {
+      console.log(n[i]);
+      //append blue circle at the end of each node
+      const nodeX = d3.select(n[i]).node().getBBox().x;
+      const nodeY = d3.select(n[i]).node().getBBox().y;
+      d3.select(n[i])
+        .append('circle')
+        .attr('r', 10)
+        .attr('fill', 'white')
+        .attr('stroke', 'blue')
+        .attr('cx', nodeX + 25)
+        .attr('cy', nodeY + 18);
+      // append exclamation mark in the circle
+      d3.select(n[i])
+        .append('text')
+        .attr('x', nodeX + 23)
+        .attr('y', nodeY + 24)
+        .attr('fill', 'blue')
+        .text('!');
+    });
   };
 }
